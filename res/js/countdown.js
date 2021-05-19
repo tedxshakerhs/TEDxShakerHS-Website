9da38587fb6bd9e40b928e6de0fa525b801afa7b
sabavdekar <sabavdekar@gmail.com>
2021-05-19 09:22:56 -0400

    HomePictures
    HomePictures

--- res/js/countdown.js
+++ res/js/countdown.js
@@ -1,5 +1,5 @@
 $('.countdown-timer').countdown({
-    date: "May 21, 2021 10:00:00",
+    date: "June 11, 2021 19:00:00",
 
     render: function(data) {
         $(".days .value").html(this.leadingZeros(data.days, 2));
